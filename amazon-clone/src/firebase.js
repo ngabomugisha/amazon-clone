8cff2dc49ab370bc575324e0378bfe0c38205bb7
NGABO MUGISHA ROBERT <ngabo.mugisha.bro@gmail.com>
2020-09-09 12:45:38 +0200

    working on navBar

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDYlirvR-uPwEmglKVRcLOuM7AZh-lJEHc",
+    authDomain: "clone-ce94f.firebaseapp.com",
+    databaseURL: "https://clone-ce94f.firebaseio.com",
+    projectId: "clone-ce94f",
+    storageBucket: "clone-ce94f.appspot.com",
+    messagingSenderId: "856533756364",
+    appId: "1:856533756364:web:2c50a27c9a154fbe011569",
+    measurementId: "G-QVB11K0RH0"
+  };
